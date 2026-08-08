0293535c3419106f500f282b9dd8beaaf7859a79
SiMoredev <seryoga287@gmail.com>
2026-08-08 15:53:24 +0300

    Добавлена скидка в main c правильной логикой

--- src/models/order.py
+++ src/models/order.py
@@ -2,7 +2,7 @@ from src.models.product import Product
 
 class Order:
 
-    def __init__(self, user, products, total, discount=0):
+    def __init__(self, user, products, total, discount=0.0):
         self.user = user
         self.products = products
         self.total = total
@@ -11,7 +11,7 @@ class Order:
     def __str__(self) -> str:
         return f"Заказ пользователя {self.user.name} на сумму {self.total} руб."
 
-    def add_product(self, product, discount):
+    def add_product(self, product, discount=0.0):
         if not isinstance(product, Product):
             raise TypeError("объект не является экземпляром класса Product")
         self.products.append(product)
